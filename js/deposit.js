23c3cb29b3a91bf235ffa887b8520a87a1b29e10
ASMRSAEED <asmrsaeed@gmail.com>
2023-02-19 01:53:56 +0600

    set-user-deposit

---
+++ js/deposit.js
@@ -0,0 +1,8 @@
+// Add Click Handler to the deposit button
+document.getElementById('btn-deposit').addEventListener('click', function () {
+
+    //Get the amount from field
+    const depositField = document.getElementById('deposit-field').value;
+    //Get the main deposit
+    const depositTotalElement = document.getElementById('deposit-total').innerText = depositField;
+    })
