db62cab2ffd8e10f3a21f31c70c77f71493b9aca
ASMRSAEED <asmrsaeed@gmail.com>
2023-02-19 00:32:18 +0600

    validate-email-password

---
+++ js/login.js
@@ -0,0 +1,16 @@
+// Add Click Handler with the submit Button
+document.getElementById('btn-submit').addEventListener('click', function () {
+    // Get the user email from the input field
+    const email = document.getElementById('user-email').value
+
+    // Get the user password from the input field
+    const password = document.getElementById('user-password').value
+
+    // Verify email and password
+    if (email == 'asmrsaeed@gmail.com' && password == 12345) {
+        console.log('correct');
+    } else {
+        alert('Invalid User')
+    }
+
+})
