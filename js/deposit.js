0cae4336fbe3be831d409e78babf5a6bcd925e06
ASMRSAEED <asmrsaeed@gmail.com>
2023-02-19 14:35:30 +0600

    make-condition

--- js/deposit.js
+++ js/deposit.js
@@ -5,23 +5,25 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     //Get the amount from field
     const depositField = document.getElementById('deposit-field');
     const newDepositAmount = parseFloat(depositField.value)
-    //Get the current deposit
-    const depositTotalElement = document.getElementById('deposit-total');
-    const previousDepositTotal = parseFloat(depositTotalElement.innerText);
-    //Update the total amount
-    const currentDepositTotal = previousDepositTotal + newDepositAmount;
-    //Set the update Amount
-    depositTotalElement.innerText = currentDepositTotal;
-    ////Get the main balance
-    const balanceTotalElement = document.getElementById('balance-total');
-    const previousBalanceTotal = parseFloat(balanceTotalElement.innerText);
-    //Update the main balance
-    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
-    //Set the main balance
-    balanceTotalElement.innerText = currentBalanceTotal
-
-
-    //Clear the deposit field
-    depositField.value = '';
+    if (newDepositAmount < 1) {
+        alert('Please Deposit Positive Amount')
+    } else {
+        //Get the current deposit
+        const depositTotalElement = document.getElementById('deposit-total');
+        const previousDepositTotal = parseFloat(depositTotalElement.innerText);
+        //Update the total amount
+        const currentDepositTotal = previousDepositTotal + newDepositAmount;
+        //Set the update Amount
+        depositTotalElement.innerText = currentDepositTotal;
+        ////Get the main balance
+        const balanceTotalElement = document.getElementById('balance-total');
+        const previousBalanceTotal = parseFloat(balanceTotalElement.innerText);
+        //Update the main balance
+        const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+        //Set the main balance
+        balanceTotalElement.innerText = currentBalanceTotal
+        //Clear the deposit field
+        depositField.value = '';
+    }
 
 })
